46840b6eda5491749f633746bdcb874795870417
CSteigueber <c.steigueber@gmail.com>
2019-12-05 14:31:04 +0100

    drinks and ood available

--- Simple_order_form/index.php
+++ Simple_order_form/index.php
@@ -14,18 +14,23 @@ function whatIsHappening() {
     var_dump($_SESSION);
 }
 //your products with their price.
-$products = [
-    ['name' => 'Club Ham', 'price' => 3.20],
-    ['name' => 'Club Cheese', 'price' => 3],
-    ['name' => 'Club Cheese & Ham', 'price' => 4],
-    ['name' => 'Club Chicken', 'price' => 4],
-    ['name' => 'Club Salmon', 'price' => 5]
-];
-$products = [
+$list=$_GET["food"];
+if ($list==1){
+    $products = [
+        ['name' => 'Club Ham', 'price' => 3.20],
+        ['name' => 'Club Cheese', 'price' => 3],
+        ['name' => 'Club Cheese & Ham', 'price' => 4],
+        ['name' => 'Club Chicken', 'price' => 4],
+        ['name' => 'Club Salmon', 'price' => 5]
+    ];
+}
+else {
+    $products = [
     ['name' => 'Cola', 'price' => 2],
     ['name' => 'Fanta', 'price' => 2],
     ['name' => 'Sprite', 'price' => 2],
     ['name' => 'Ice-tea', 'price' => 3],
-];
+    ];
+}
 $totalValue = 0;
 require 'form-view.php';
