1e41c3085a005dad94909aabac59b53dea977b13
CSteigueber <c.steigueber@gmail.com>
2019-12-11 16:34:56 +0100

    battles included

--- Class_conflict/Characters.php
+++ Class_conflict/Characters.php
@@ -2,27 +2,47 @@
 class humanoid{
     public $name;
     public $health=100;
-    public $speed;
+    public $speed=5;
     public $legs=2;
     public $hands=2;
     public $heads=1;
     public $intellegence;
     public $age;
-    public $strength;
+    public $strength=5;
 
-    public function __construct ($name="random humanoid",$age){
+    public function validate($target){
+        echo "validation:</br>";
+        echo $target->name. " health: ".$target->health."</br>";
+        if ($target->health<=0){
+            echo $target->name." died.</br>";
+        }
+        if ($target->health<=-100){
+            echo "What an overkill!";
+        }
+    }
+
+    public function __construct ($name="random humanoid",$age=0){
         $this->age=$age;
         $this->name=$name;
     }
-    public function set_strength($strength){
-        $this->strength=$strength;
-    }
     public function set_atr($atr,$value){
-        $this ::$$atr=$value;
+        $this ->$atr=$value;
+        echo $atr." from ".$this->name."is now ".$value;
     }
-
+    public function increase_atr($atr,$value){
+        $this->$atr+=$value;
+        echo $this->name." ".$atr." increased by ".$value;
+        echo "</br>".$atr." is now ".$this->$atr;
+    }
+    public function attack($target){
+        $damage=$this->strength * rand(1,100)/100;
+        $target->health-=$damage;
+        echo "BAAM! ".$this->name." attacked ".$target->name."</br>";
+        echo $damage." damage caused. </br>";
+        $this->validate($target);
+    }
 }
-class homo_sapiens extends humanoid{
+class human extends humanoid{
     public $intellegence =50;
     public $job='random job';
     public $education='random education';
--- Class_conflict/index.php
+++ Class_conflict/index.php
@@ -1,7 +1,34 @@
 <?php
+function battle ($player1,$player2){
+    do {
+    if ($player1->health>0){
+        $player1->attack($player2);
+    }
+    if ($player2->health>0){
+        $player2->attack($player1);
+    }
+    echo "</br></br>";
+}
+while ($player1->health>0 && $player2->health>0);
+}
+
+echo "Game on!</br>";
 require 'Characters.php';   
-$claas= new homo_sapiens("Claas", 34);
-$claas->set_strength(66);
-$claas->set_atr("health",200);
-echo "Game on!";
-var_dump($claas);
+$claas= new human("Claas");
+$enemy01=new human("dude",6);
+$claas->set_atr("strength",10);
+battle($claas,$enemy01);
+echo "</br></br>";
+$enemy02=new human("Stijn");
+battle($claas,$enemy02);
+echo "</br></br>";
+echo "</br></br>Claas is taking a rest to recover. ";
+$claas->increase_atr("health",50);
+$enemy03= new human ("Jasper");
+$enemy03->set_atr("strength", 8);
+battle($enemy03,$claas);
+echo "</br></br>";
+
+
+
+echo "</br>Game over!";
